4a58e23bd5f21d1b761b43e692b11150ab8c72b0
Darius Tiganas <adarius9.1999@gmail.com>
2025-07-17 12:50:41 +0200

    Initial commit

---
+++ src/main/java/com/darius/tummyhurtmod/TummyHurtMod.java
@@ -0,0 +1,53 @@
+package com.darius.tummyhurtmod;
+
+import com.mojang.logging.LogUtils;
+import net.minecraft.client.Minecraft;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
+import net.minecraftforge.eventbus.api.listener.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(TummyHurtMod.MOD_ID)
+public final class TummyHurtMod {
+    // Define mod id in a common place for everything to reference
+    public static final String MOD_ID = "tummyhurtmod";
+    // Directly reference a slf4j logger
+    public static final Logger LOGGER = LogUtils.getLogger();
+
+    public TummyHurtMod(FMLJavaModLoadingContext context) {
+        var modBusGroup = context.getModBusGroup();
+        // Register the commonSetup method for modloading
+        FMLCommonSetupEvent.getBus(modBusGroup).addListener(this::commonSetup);
+        // Register the item to a creative tab
+        BuildCreativeModeTabContentsEvent.getBus(modBusGroup).addListener(TummyHurtMod::addCreative);
+
+        // Register our mod's ForgeConfigSpec so that Forge can create and load the config file for us
+        context.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+
+    }
+
+
+    private static void addCreative(BuildCreativeModeTabContentsEvent event) {
+
+    }
+
+    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
+    @Mod.EventBusSubscriber(modid = MOD_ID, value = Dist.CLIENT)
+    public static class ClientModEvents {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event) {
+            // Some client setup code
+            LOGGER.info("HELLO FROM CLIENT SETUP");
+            LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
+        }
+    }
+}
